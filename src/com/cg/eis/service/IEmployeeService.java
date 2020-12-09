8f3ab6b7d4cb90abecb1742fd1e67e4b8d188fa3
Theja <thejafromtvp@gmail.com>
2020-12-09 22:19:35 +0530

    LabAssignment

---
+++ src/com/cg/eis/service/IEmployeeService.java
@@ -0,0 +1,11 @@
+package com.cg.eis.service;
+import com.cg.eis.bean.Employee;
+public interface IEmployeeService {
+	public void addEmployee(Employee emp);
+
+	public String findInsuranceScheme(Employee emp);
+
+	public void displayEmployeeDetails(String insuranceScheme);
+
+	public boolean deleteEmployee(long employeeId);
+}
